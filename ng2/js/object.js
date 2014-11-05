8fe61c5d26ace85e34a2b243f4f28a6d5caa34c7
paysonwelch <payson.welch@nbtsolutions.com>
2014-11-05 14:53:59 -0500

    quick example of using jasmine and karma with PhantomJS

---
+++ ng2/js/object.js
@@ -0,0 +1,16 @@
+﻿if (typeof NS == 'undefined') { NS = {}; }
+
+NS.myFunction = {    
+    stuff: [],
+    myInit: function myInit() {
+        this.stuff.push('Testing');
+    },
+    reset: function reset() {
+        this.stuff = [];        
+    },
+    append: function append(string1, string2) {
+        return string1 + string2;
+    }
+};
+
+NS.myFunction.myInit();
---
+++ ng2/js/tests/object.test.js
@@ -0,0 +1,35 @@
+﻿describe("myFunction", function () {
+    var myfunc = NS.myFunction;
+
+    beforeEach(function () {
+        spyOn(myfunc, 'myInit');
+    });
+
+    afterEach(function () {       
+        myfunc.stuff = [];
+        myfunc.stuff.push('Testing');
+    }, false);
+    
+    it("should be able to initialize", function () {        
+        expect(myfunc.myInit).toBeDefined();
+        myfunc.myInit();
+        expect(myfunc.myInit).toHaveBeenCalled();
+    });
+        
+    it("should populate stuff during initialization", function () {
+
+        expect(myfunc.stuff.length).toEqual(1);
+        expect(myfunc.stuff[0]).toEqual('Testing');
+    });
+
+    describe("appending string", function () {
+        it("should be able to append 2 string", function () {
+            expect(myfunc.append).toBeDefined();
+        });
+        it("should append 2 string", function () {
+            expect(myfunc.append('hello ', 'world')).toEqual('hello world');
+        });
+    });
+});
+
+
